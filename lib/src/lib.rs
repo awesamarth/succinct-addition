979db172dbf915a427e97794bf5351ad574deec0
Samarth Saxena <samarthsaxena1672003@gmail.com>
2025-05-07 11:14:37 +0000

    lib file changed

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -8,14 +8,3 @@ sol! {
         uint32 sum;
     }
 }
-
-/// Compute the sum of two numbers (wrapping around on overflows), using normal Rust code.
-pub fn add(a: u32, b: u32) -> u32 {
-    a.wrapping_add(b)
-}
-
-/// Test function to verify the addition is correct
-pub fn test_addition(a: u32, b: u32) -> bool {
-    let sum = add(a, b);
-    sum == a.wrapping_add(b)
-}
